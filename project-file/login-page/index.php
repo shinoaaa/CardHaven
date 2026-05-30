43b1a9581786ca3fcc1b2ca68f36c3b4658cadd3
Wandererssss <nabilynwa@gmail.com>
2026-05-30 18:44:14 +0700

    Ini nambah template login dan regis

---
+++ project-file/login-page/index.php
@@ -0,0 +1,22 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+    <link rel="stylesheet" href="../global.css">
+    <link rel="stylesheet" href="style.css">
+</head>
+<body>
+    <div class="container">
+        <div class="card">
+            <div style="width: 45%; height: 100%; display: flex; justify-content: center;">
+                <img src="../../assets/image/login-image.svg" style="object-fit: cover; height: 100%;">
+            </div>
+            <div style="background-color: red; width: 55%; height: 100%;">
+                
+            </div>
+        </div>
+    </div>
+</body>
+</html>
