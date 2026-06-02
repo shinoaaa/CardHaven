f365f8ba3cd18ca394b99717157586fab774dcdd
Wandererssss <nabilynwa@gmail.com>
2026-06-02 08:33:33 +0700

    Nambah koneksi

---
+++ connection.php
@@ -0,0 +1,19 @@
+<?php 
+    $serverName = '100.113.106.47';
+    $serverProp = [
+        'database' => 'CardHaven',
+        'UID' => 'sa',
+        'PWD' => 'Admin123!',
+        'TrustServerCertificate' => true
+    ];
+
+    $conn = sqlsrv_connect($serverName,$serverProp);
+
+    if(!$conn){
+        echo 'error jir';
+        die(print_r(sqlsrv_errors(), true));
+    }
+    else{
+        echo "lesgo";
+    }
+?>
