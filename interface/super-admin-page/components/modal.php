91e1a255663fa904b744f0adb9716e15861e74a9
Rifqi Zahran <rifqizahran1078@gmail.com>
2026-06-09 08:47:58 +0700

    tes comment

--- interface/super-admin-page/components/modal.php
+++ interface/super-admin-page/components/modal.php
@@ -108,7 +108,7 @@
                 </div>
                 <div class="status-text">Current Status: <span id="pStatusLabel"></span></div>
                 <input type="hidden" name="status" id="pStatusValue">
-            </div>
+            </div> //ireng
 
             <button type="submit" class="btn-confirm">Confirm Product</button>
         </form>
